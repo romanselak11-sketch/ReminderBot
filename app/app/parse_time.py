6b3ea0e0a0f295bef279f420b74edfa67e5a6e2b
Roman Lagutenko <pushkinroma11@mail.ru>
2025-08-24 23:56:38 +0500

    fix багов

--- app/app/parse_time.py
+++ app/app/parse_time.py
@@ -27,8 +27,8 @@ async def parse_text_in_date(text: str):
     result_date, current_date = datetime.now(), datetime.now()
     change_text.text = text.strip().lower()
     result_time = None
-    dd_mm_yyyy = await _parse_dd_mm_yyy(change_text.text)
-    hh_mm_ss = await _parse_time(change_text.text)
+    dd_mm_yyyy = await _parse_dd_mm_yyy()
+    hh_mm_ss = await _parse_time()
 
     if dd_mm_yyyy:
         return dd_mm_yyyy
@@ -72,11 +72,10 @@ async def parse_text_in_date(text: str):
                     result_date = result_date
                 else:
                     result_date = result_date + timedelta(days=delta_days)
-
+        logger.info(f'Текст: {change_text.text}')
         result_date = await _parse_in_an_time(result_date)
 
         if result_time:
-            logger.info(f'Результат: {datetime.combine(result_date, result_time)}')
             return datetime.combine(result_date, result_time)
 
         if current_date != result_date:
@@ -90,73 +89,94 @@ async def parse_text_in_date(text: str):
 
 async def _parse_in_an_time(result_date: datetime):
     if 'день' in change_text.text or 'дня' in change_text.text or 'дней' in change_text.text:
-        pattern = r'через\s+(\d+)\s*(день|дня|дней)'
-        number = search(pattern, change_text.text)
+        pattern = r'через\s*(\d*)\s*(день|дня|дней)'
+        numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + timedelta(days=int(number.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + timedelta(days=1))
+            else:
+                result_date = (result_date + timedelta(days=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + timedelta(days=1))
 
     if 'недел' in change_text.text:
-        pattern = r'через\s+(\d+)\s*(неделю|недели|недель|нед)'
+        pattern = r'через\s*(\d*)\s*(неделю|недели|недель|нед)'
         numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + timedelta(weeks=int(numbers.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + timedelta(weeks=1))
+            else:
+                result_date = (result_date + timedelta(weeks=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + timedelta(weeks=1))
 
     if 'месяц' in change_text.text:
-        pattern = r'через\s+(\d+)\s*(месяц|месяца|месяцев|мес)'
+        pattern = r'через\s*(\d*)\s*(месяц|месяца|месяцев|мес)'
         numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + relativedelta(months=int(numbers.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + relativedelta(months=1))
+            else:
+                result_date = (result_date + relativedelta(months=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + relativedelta(months=1))
 
     if 'год' in change_text.text:
-        pattern = r'\s+(\d+)\s*(год|года|лет)'
+        pattern = r'\s*(\d*)\s*(год|года|лет)'
         numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + relativedelta(years=int(numbers.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + relativedelta(years=1))
+            else:
+                result_date = (result_date + relativedelta(years=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + relativedelta(years=1))
 
     if 'час' in change_text.text:
-        pattern = r'через\s+(\d+)\s*(час|часа|часов)'
+        pattern = r'через\s*(\d*)\s*(час|часа|часов)'
         numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + timedelta(hours=int(numbers.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + timedelta(hours=1))
+            else:
+                result_date = (result_date + timedelta(hours=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + timedelta(hours=1))
 
     if 'минут' in change_text.text:
-        pattern = r'через\s+(\d+)\s*(минут|минуты|минуту|мин)'
+        pattern = r'через\s*(\d*)\s*(минут|минуты|минуту|мин)\b'
         numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + timedelta(minutes=int(numbers.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + timedelta(minutes=1))
+            else:
+                result_date = (result_date + timedelta(minutes=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + timedelta(minutes=1))
 
     if 'секунд' in change_text.text:
-        pattern = r'через\s+(\d+)\s*(секунд|секунды|секунду|сек)'
+        pattern = r'через\s*(\d*)\s*(секунд|секунды|секунду|сек)'
         numbers = search(pattern, change_text.text)
         try:
-            result_date = (result_date + timedelta(seconds=int(numbers.group(1))))
+            if numbers.group(1) == '':
+                result_date = (result_date + timedelta(seconds=1))
+            else:
+                result_date = (result_date + timedelta(seconds=int(numbers.group(1))))
             change_text.update_text(change_text.text, pattern)
         except AttributeError:
             result_date = (result_date + timedelta(seconds=1))
     return result_date
 
 
-async def _parse_time(text: str):
+async def _parse_time():
     pattern = r'(?:\bв?\s+|\bво\s+)(\d{2})[:.,]?(\d{2})'
-    times = search(pattern, text)
+    times = search(pattern, change_text.text)
     if times:
         hour = int(times.group(1))
         minute = int(times.group(2)) if times.group(2) else 0
@@ -167,14 +187,14 @@ async def _parse_time(text: str):
             minute = minute % 60
             if hour >= 24:
                 return None
-            change_text.update_text(text, pattern)
+            change_text.update_text(change_text.text, pattern)
             return time(hour, minute, second=0)
     return None
 
 
-async def _parse_dd_mm_yyy(text: str):
+async def _parse_dd_mm_yyy():
     pattern = r'\b(\d{2})\.(\d{2})\.(\d{4})\s+(\d{1,2}):(\d{1,2}):(\d{1,2})\b'
-    parsed = search(pattern, text)
+    parsed = search(pattern, change_text.text)
     if parsed:
         day = parsed.group(1)
         if int(day) > 31 or int(day) < 1:
@@ -194,7 +214,7 @@ async def _parse_dd_mm_yyy(text: str):
         second = parsed.group(6)
         if int(second) < 1 or int(second) > 59:
             return False
-        change_text.update_text(text, pattern)
+        change_text.update_text(change_text.text, pattern)
         return datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
     else:
         return False
