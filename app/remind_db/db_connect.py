715f71d0264bdf7f4e48585a492dcf23feec0ff9
Roman Lagutenko <pushkinroma11@mail.ru>
2025-08-25 15:57:45 +0500

    Добавлена проверка наличия БД перед запуском

--- app/remind_db/db_connect.py
+++ app/remind_db/db_connect.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Engine, create_engine
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy_utils import create_database
 from typing import Optional
 from config import get_sync_database_url, get_async_database_url
 from logger_config import get_logger
@@ -12,20 +13,34 @@ logger = get_logger(__name__)
 class ReminderDB:
     def __init__(self):
         self._engine: Optional[Engine] = None
-        try:
+        logger.info('Проверяем наличие БД')
+        if not self._get_db():
+            logger.info('БД Не найдена')
             logger.info('Создаём БД')
             create_database(get_sync_database_url())
-        except Exception as e:
-            logger.error(f"БД уже создана или возникла ошибка при создании: {e}")
-            pass
+            try:
+                logger.info('Создаём таблицы')
+                sync_engine = create_engine(get_sync_database_url())
+                Base.metadata.create_all(sync_engine)
+                sync_engine.dispose()
+            except Exception as e:
+                logger.error(f"Таблицы уже созданы или возникла ошибка при создании: {e}")
+        else:
+            logger.error(f"БД найдена")
+            self._engine.dispose()
+
+    def _get_db(self):
         try:
-            logger.info('Создаём таблицы')
-            sync_engine = create_engine(get_sync_database_url())
-            Base.metadata.create_all(sync_engine)
-            sync_engine.dispose()
+            self._engine = create_engine(get_sync_database_url())
+            with self._engine.connect() as conn:
+                pass
+            self._engine.dispose()
+            return True
+        except OperationalError:
+            return False
         except Exception as e:
-            logger.error(f"БД уже создана или возникла ошибка при создании: {e}")
-            pass
+            logger.error(f'При поиске БД возникла ошибка: {e}')
+            return False
 
     def get_engine(self):
         if self._engine is None:
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from app import app
+from logger_config import get_logger
+
+logger = get_logger(__name__)
 
 
 if __name__ == '__main__':
+    logger.info('Запуск приложения')
     pass
 
 
